0bee63dbf8c3721f9dfe867e5aa8a6bd28729b9e
Sayandip Bhattacharya <shayon83@gmail.com>
2025-04-20 17:41:36 -0700

    chore(pre-commit): add pre-commit configs

--- terraform.tf
+++ terraform.tf
@@ -1,4 +1,11 @@
 terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+  }
   cloud {
     organization = "SayanOrg"
 
